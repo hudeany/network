a02f22303cb36ae8a68b2393c2a9de840e5b1609
Andreas Soderer <git@soderer.de>
2025-12-15 21:12:29 +0100

    Added constants

--- src/main/java/de/soderer/network/HttpConstants.java
+++ src/main/java/de/soderer/network/HttpConstants.java
@@ -3,5 +3,11 @@ package de.soderer.network;
 public class HttpConstants {
 	public static final String HTTPHEADERNAME_CONTENTLENGTH = "Content-Length";
 	public static final String HTTPHEADERNAME_CONTENTTYPE = "Content-Type";
+	public static final String HTTPHEADERNAME_DISPOSITION  = "Content-Disposition ";
 
+	public static final String HTTPHEADERNAME_COOKIE = "Cookie";
+	public static final String HTTPHEADERNAME_AUTHORIZATION = "Authorization";
+
+	public static final String AUTHORIZATIONHEADER_START_BEARER = "Bearer";
+	public static final String AUTHORIZATIONHEADER_START_BASIC = "Basic";
 }
